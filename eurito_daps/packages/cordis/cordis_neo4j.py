f7b6fd1cf7c756a12733173b9dc23c61fd816c03
Joel Klinger <joel.alexander.klinger@gmail.com>
2019-09-11 19:03:18 +0100

    refactored

--- eurito_daps/packages/cordis/cordis_neo4j.py
+++ eurito_daps/packages/cordis/cordis_neo4j.py
@@ -1,5 +1,3 @@
-from neo4j.exceptions import CypherError
-
 from py2neo import Graph, NodeMatcher
 from py2neo.data import Node, Relationship
 from sqlalchemy.inspection import inspect
@@ -13,7 +11,86 @@ from nesta.core.orms.cordis_orm import Base, Project
 import logging
 
 
-def flatten(data):
+def orm_to_neo4j(db, tx, parent, row, this, rel):
+    row = flatten(row)
+    if rel is not None:
+        fwd_rel, back_rel = build_relationships(db=db, graph=tx.graph,
+                                                parent=parent, row=row,
+                                                this=this, rel=rel)
+        # Both nodes in the relationship were found
+        if (fwd_rel, back_rel) != (None, None):
+            tx.create(fwd_rel)
+            tx.create(back_rel)
+    else:
+        set_constraints(this, tx.graph.schema)
+        tx.create(Node(extract_name(this.__tablename__), **row))
+
+
+def build_relationships(db, graph, parent, row, this, rel):
+    # Case 1) `this` is a node
+    if parent is None:
+        this_node = Node(extract_name(this.__tablename__), **row)
+        rel_props = {}
+    # Case 2) `this` is a relationship
+    else:
+        this_node = retrieve_node(db, graph, parent, row, this)
+        rel_props = row
+    # Also retrieve the Project node from neo4j
+    proj_node = retrieve_node(db, tx.graph, Project, row, this)
+    # If either node is not found, give up
+    if proj_node is None or this_node is None:
+        return None, None
+    # Build a forward and backward relationship, wrt the project
+    relationship = Relationship(proj_node, rel, this_node, **rel_props)
+    back_relationship = Relationship(this_node, 'HAS_PROJECT', proj_node)
+    return relationship, back_relationship
+
+
+def set_constraints(orm, graph_schema):
+    entity_name = extract_name(orm.__tablename__)
+    (pk,) = inspect(orm).primary_key
+    constrs = graph_schema.get_uniqueness_constraints(entity_name)
+    assert len(constrs) <= 1
+    if len(constrs) == 0:
+        logging.info('Creating constraint on '
+                     f'{entity_name}.{pk.name}')
+        graph_schema.create_uniqueness_constraint(entity_name,
+                                                  pk.name)
+    else:
+        assert len(constrs[0]) == 1
+
+
+def prepare_base_entities(table):
+    """Returns the objects required to generate a graph
+    representation of the ORM.
+
+    Args:
+        table (sqlalchemy.sql.Table): SQL alchemy table object from which
+                                      to extract an graph representation.
+    Returns:
+        {this, parent, rel} ({Base, Base, str}): Two ORMs and a string
+                                                 describing their relationship
+    """
+    fks = [fk for c in table.columns
+           if c.foreign_keys
+           for fk in c.foreign_keys]
+    this = get_class_by_tablename(Base, table.name)
+    parent, rel = None, None
+    if len(fks) == 1:
+        rel = f'HAS_{entity_name.upper()}'
+    elif len(fks) == 2:
+        _tablename = table_from_fk(fks)
+        rel = f'HAS_{extract_name(_tablename).upper()}'
+        parent = get_class_by_tablename(Base, _tablename)
+    return this, parent, rel
+
+
+def flatten(row):
+    row = object_to_dict(row, shallow=True)
+    return {k: _flatten(v) for k, v in row.items()}
+
+
+def _flatten(data):
     if type(data) is dict:
         return flatten_dict(data)
     elif type(data) is not list:
@@ -72,13 +149,12 @@ def get_row(session, _class, row, this):
                   if fk.column.table.name == _class.__tablename__]
     this_value = row[this_pk.name]
     _row = session.query(_class).filter(pk == this_value).first()
-    _row = object_to_dict(_row, shallow=True)
-    return {k: flatten(v) for k, v in _row.items()}
+    return flatten(_row)
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    limit = None
+    limit = 10
 
     engine = get_mysql_engine('MYSQLDB', 'nesta', 'production')
     conf = get_config('neo4j.config', 'neo4j')
@@ -86,7 +162,7 @@ if __name__ == '__main__':
                    auth=(conf['user'], conf['password']))
 
     with graph_session(**gkwargs) as tx:
-        logging.info('Dropping all...')
+        logging.info('Dropping all previous data')
         tx.graph.delete_all()
         for constraint in tx.run('CALL db.constraints'):
             logging.info(f'Dropping constraint {constraint[0]}')
@@ -95,54 +171,9 @@ if __name__ == '__main__':
     for tablename, table in Base.metadata.tables.items():
         entity_name = extract_name(tablename)
         logging.info(f'\tProcessing {entity_name}')
-        fks = [fk for c in table.columns
-               if c.foreign_keys
-               for fk in c.foreign_keys]
-        this = get_class_by_tablename(Base, tablename)
-        parent, rel = None, None
-        if len(fks) == 1:
-            rel = f'HAS_{entity_name.upper()}'
-        elif len(fks) == 2:
-            _tablename = table_from_fk(fks)
-            rel = f'HAS_{extract_name(_tablename).upper()}'
-            parent = get_class_by_tablename(Base, _tablename)
-
+        this, parent, rel = prepare_base_entities(table)
         with graph_session(**gkwargs) as tx:
-            for db, row in db_session_query(query=this,
-                                            engine=engine,
+            for db, row in db_session_query(query=this, engine=engine,
                                             limit=limit):
-                row = object_to_dict(row, shallow=True)
-                row = {k: flatten(v) for k, v in row.items()}
-                #row = {k: v for k, v in row.items()
-                #       if type(v) not in (dict, list)}
-                obj = Node(entity_name, **row)
-                if rel is not None:
-                    rel_props = {}
-                    if parent is not None:
-                        obj = retrieve_node(db, tx.graph, parent,
-                                            row, this)
-                        rel_props = row
-                    proj_node = retrieve_node(db, tx.graph, Project,
-                                              row, this)
-                    if proj_node is None or obj is None:
-                        continue
-                    back_rel = Relationship(obj, 'HAS_PROJECT', proj_node)
-                    tx.create(back_rel)
-                    obj = Relationship(proj_node, rel, obj, **rel_props)
-                else:
-                    s = tx.graph.schema
-                    (pk,) = inspect(this).primary_key
-                    constrs = s.get_uniqueness_constraints(entity_name)
-                    assert len(constrs) <= 1
-                    if len(constrs) == 0:
-                        logging.info('\t\t\tCreating constraint on '
-                                     f'{entity_name}.{pk.name}')
-                        s.create_uniqueness_constraint(entity_name,
-                                                       pk.name)
-                    else:
-                        assert len(constrs[0]) == 1
-                try:
-                    tx.create(obj)
-                except CypherError:
-                    print(obj)
-                    raise
+                orm_to_neo4j(db=db, tx=tx, parent=parent,
+                             row=row, this=this, rel=rel)
