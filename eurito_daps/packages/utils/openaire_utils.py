d92ee9e337e3e8aeb9565d5fe2111a3cdbe304f8
Agzam Idrissov <agzid@dtu.dk>
2019-11-05 09:13:53 +0000

    now taking records directly from neo4j

--- eurito_daps/packages/utils/openaire_utils.py
+++ eurito_daps/packages/utils/openaire_utils.py
@@ -1,38 +1,117 @@
 from bs4 import BeautifulSoup
 import re
 from eurito_daps.core.orms import openaire_orm
+from eurito_daps.core.orms.openaire_orm import Base, SoftwareRecord, association_table
 import pdb
 import logging
 import time
+from py2neo import Node, NodeMatcher
 
-
-def link_record_with_project(record, record_obj, db_session):
-    '''A utility function, which links records with related EC projects and stores this linkage in the association table in the database
+def write_record_to_neo(record, output_type, graph):
+    '''A utility function, which takes record and writes it to neo4j graph
 
     Args:
-        record (dict): contains metadata of a record (e.g. record of type "software" would store fields such as "title", "pid", "creator", etc.)
-        record_obj (ORM Object): a record returned by a query from the database (could be software, dataset, publication or ECProject record)
+        record (dict): a dictionary that contains metadata about a record
+        output_type(str): type of record to be extracted from OpenAIRE API. Accepts "software", "datasets", "publications", "ECProjects"
+        graph(graph_session): connection to neo4j database
     '''
-    #extract project codes
-    project_codes = record['project_codes']
-    #for each project code, create projectcode object,
-    #add a relationship between this projectcode and softwareObj in association table
-    for project_code in project_codes:
-        #find project with this code and create an object
-        #pdb.set_trace()
-        project_obj = find_project_in_db(project_code, db_session)
-        #if there is a related project, create a relationship between current software and found project
-        if project_obj:
-            project_obj.software.append(record_obj)
 
-    return record_obj
+    record_type = str(output_type).capitalize()
+
+    found_node = graph.nodes.match(output_type, pid=record['pid']).first()
+
+    if found_node == None:
+        created_node = Node(record_type, title=record['title'], pid=record['pid'])
+        graph.create(created_node)
+        return created_node
+    else:
+        logging.info("returning found node")
+        return found_node
+
+def get_project_soups(currentUrl, reqsession, output_type, projectID):
+    ''' Gets a beautiful soup according to output type and projectID
+
+        Args:
+            currentUrl(str): URL to OpenAIRE API
+            reqsession (instance of Requests session): currently open HTTP request
+            output_type(str): type of record to be extracted from OpenAIRE API. Accepts "software", "datasets", "publications", "ECProjects"
+            projectID(str): EC project identifier
+
+        Returns:
+            souplist(list): a list of BeautifulSoup objects that contain the results from API call
+    '''
+
+    response_size = -1
+    total = 9999
+    page = 1
+    pagesize = 100
+    souplist = list()
+    while (page + 1)*pagesize < total:
+        response = reqsession.get(currentUrl + output_type, params={'hasProject': 'true', 'projectID': projectID, 'size': pagesize, 'page': page})
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'lxml')
+            #logging.info("soup retrieved for page %d" % page)
+            souplist.append(soup)
+            response.close()
+            if page == 1:
+                total = int(soup.find("total").text)
+                #logging.info("total: %d" % total)
+            page += 1
+        else:
+            logging.info(response.status_code)
+            logging.info("Service unavailable, waiting 10 seconds and trying again")
+            time.sleep(10)
+            continue
+    return souplist
+
+def get_results_from_soups(souplist):
+    ''' Extracts string from all BeautifulSoup objects and merges them into one list
+
+        Args:
+            souplist(list): a list of BeautifulSoup objects that contain the results from API call
+
+        Returns:
+            resultlist(list): a list of strings with results metadata
+    '''
+    resultlist = list()
+    for soup in souplist:
+        soupresults = soup.find_all("oaf:result")
+        resultlist = resultlist + soupresults
+    return resultlist
+
+'''
+def get_soup_contents(currentUrl, reqsession, output_type, resumption_token):
+
+    for x in range(0, 9):
+        #requests.get(url, params={'metadataPrefix':'oaf', 'set':output_type})
+        #pdb.set_trace()
+        if resumption_token == 'None' or resumption_token == 'First request':
+            response = reqsession.get(currentUrl, params={'verb': 'ListRecords', 'metadataPrefix': 'oaf', 'set': output_type})
+            logging.info("No resumptionToken")
+        else:
+            logging.info("resumptionToken is there ")
+            #response = reqsession.get(currentUrl, params={'verb': 'ListRecords', 'metadataPrefix': 'oaf', 'set': output_type, 'resumptionToken': resumption_token})
+            #resumptionToken as a parameter does not work for requests call, hence use string request
+            requeststr = currentUrl + '?verb=ListRecords&resumptionToken=' + resumption_token
+            logging.info(requeststr)
+            response = reqsession.get(requeststr)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'lxml')
+            response.close()
+            return soup
+        else:
+            logging.info(response.status_code)
+            logging.info("Service unavailable, waiting 10 seconds and trying again")
+            time.sleep(10)
+            continue
+
+def add_linkages_to_neo(record, output_type, graph):
+
+    cypherquery = " MATCH (b:Project),(a:Dataset) WHERE a.id = %s AND ID(b) = %s CREATE (b)-[r:hasDataset]->(a) RETURN r" % (project_codes, record['id'])
+
+    graph.run(cypherquery)
 
 def find_project_in_db(in_project_code, db_session):
-    '''A utility function, which returns ORM-type record objects from the database with the specified EC project code
-
-    Args:
-        in_project_code (int): EC 6-digit project code
-    '''
 
     records = db_session \
                      .query(openaire_orm.ECProjectRecord) \
@@ -46,13 +125,6 @@ def find_project_in_db(in_project_code, db_session):
         return None
 
 def write_records_to_db(records, output_type, db_session):
-    '''A utility function, which writes specified records into the database
-
-    Args:
-        records (list of dicts): stores metadata of records (e.g. record of type "software" would store fields such as "title", "pid", "creator", etc.)
-        output_type (str): type of record to be extracted from OpenAIRE API. Accepts "software", "datasets", "publications", "ECProjects"
-        db_session (instance of sessionmaker Session): current database session
-    '''
 
     if output_type == "software":
         is_software = True
@@ -75,25 +147,13 @@ def write_records_to_db(records, output_type, db_session):
         db_session.commit()
 
 def get_record_object(cur_record, output_type):
-    '''A utility function, which returns a ORM-type object according to the specified output type
 
-    Args:
-        record (dict): contains metadata of a record (e.g. record of type "software" would store fields such as "title", "pid", "creator", etc.)
-        output_type (str): type of record to be extracted from OpenAIRE API. Accepts "software", "datasets", "publications", "ECProjects"
-    '''
     if output_type == 'software':
         return openaire_orm.SoftwareRecord(title=cur_record['title'], pid=cur_record['pid'], creators=str(cur_record['creators']) )
     if output_type == 'ECProjects':
         return openaire_orm.ECProjectRecord(title=cur_record['title'], project_code=cur_record['project_code'])
 
-#TODO add research datasets and other research projects parser
-
 def parse_soft (cur_soup):
-    '''A utility function, which parses software records from XML and returns a list of records with software that are related to EC Projects
-
-    Args:
-        cur_soup (XML string): contains string formatted in XML, that was obtained from BeautifulSoup request to the API
-    '''
     output_list = list()
     results = cur_soup.find_all(re.compile("^oaf:result"))
 
@@ -105,11 +165,7 @@ def parse_soft (cur_soup):
          if r.find_all('code')] #if code tag exists, then it is related to EC project
 
 def parse_proj (cur_soup):
-    '''A utility function, which parses EC project records from XML, returns a list of records
 
-    Args:
-        cur_soup (XML string): contains string formatted in XML, that was obtained from BeautifulSoup request to the API
-    '''
     output_list = list()
     results = cur_soup.find_all(re.compile("^oaf:project"))
 
@@ -117,14 +173,17 @@ def parse_proj (cur_soup):
          'project_code': r.find('code').text}
          for r in results]
 
+def parse_datasets (cur_soup):
+    output_list = list()
+    results = cur_soup.find_all(re.compile("^oaf:result"))
+
+    return [{'project_codes': r.find('code').text,
+        'title': r.find('title').text,
+        'pid': r.find('pid').text,}
+         for r in results
+         if r.find_all('code')] #if code tag exists, then it is related to EC project
 
 def get_res_token(soup):
-    '''A utility function, which extracts resumption token from XML, returns a string containing resumption token
-
-    Args:
-        soup (XML string): contains string formatted in XML, that was obtained from BeautifulSoup request to the API
-    '''
-
     with open('current_soup.txt', 'w',  encoding="utf-8") as f:
             f.write(str(soup))
     res_token = soup.find(re.compile("^oai:resumptiontoken"))
@@ -136,33 +195,4 @@ def get_res_token(soup):
         return res_token_str
     else:
         return 'None'
-
-def get_soup_contents(currentUrl, reqsession, output_type, resumption_token):
-    '''A utility function, which returns BeautifulSoup content in XML format from the given URL
-
-    Args:
-        currentUrl (string): contains API request URL
-        reqsession (instance of Requests session): currently open HTTP request
-    '''
-
-    for x in range(0, 9):
-        #requests.get(url, params={'metadataPrefix':'oaf', 'set':output_type})
-        #pdb.set_trace()
-        if resumption_token == 'None' or resumption_token == 'First request':
-            response = reqsession.get(currentUrl, params={'verb': 'ListRecords', 'metadataPrefix': 'oaf', 'set': output_type})
-            logging.info("No resumptionToken")
-        else:
-            logging.info("resumptionToken is there ")
-            #response = reqsession.get(currentUrl, params={'verb': 'ListRecords', 'metadataPrefix': 'oaf', 'set': output_type, 'resumptionToken': resumption_token})
-            #resumptionToken as a parameter does not work for requests call, hence use string request
-            requeststr = currentUrl + '?verb=ListRecords&resumptionToken=' + resumption_token
-            logging.info(requeststr)
-            response = reqsession.get(requeststr)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.content, 'lxml')
-            response.close()
-            return soup
-        else:
-            logging.info("Service unavailable, waiting 10 seconds and trying again")
-            time.sleep(10)
-            continue
+'''
